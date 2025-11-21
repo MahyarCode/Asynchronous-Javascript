754252f2de97308679a3782ce7818596339992b9
Mahyar Mousavinia <mahyar.mousavi.1408@gmail.com>
2025-11-21 18:00:47 +0400

    the last version before the website got shutdown; the next commit will be on another api for countires

--- script.js
+++ script.js
@@ -43,33 +43,75 @@ const renderCountryInHTML = function (data, className = '') {
 
 // getCountryInfo('usa');
 //////////////////////////////////////////////////////
-//TODO showing multiple country in a specific order (not showing each of them which gets its api faster)
-const getCountryAndNeighbour = function (country) {
-    // AJAX call country 1
-    const request = new XMLHttpRequest();
-    request.open('GET', `https://restcountries.com/v2/name/${country}`);
-    request.send();
+// //TODO showing multiple country in a specific order (not showing each of them which gets its api faster)
+// const getCountryAndNeighbour = function (country) {
+//     // AJAX call country 1
+//     const request = new XMLHttpRequest();
+//     request.open('GET', `https://restcountries.com/v2/name/${country}`);
+//     request.send();
 
-    request.addEventListener('load', function () {
-        const [data] = JSON.parse(request.responseText);
+//     request.addEventListener('load', function () {
+//         const [data] = JSON.parse(request.responseText);
 
-        // render country 1
-        renderCountryInHTML(data);
+//         // render country 1
+//         renderCountryInHTML(data);
 
-        // Get neighbor country 2
-        const neighbour = data?.borders[0];
+//         // Get neighbor country 2
+//         const neighbour = data?.borders[0];
 
-        // AJAX call country 2
-        const request2 = new XMLHttpRequest();
-        request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
-        request2.send();
+//         // AJAX call country 2
+//         const request2 = new XMLHttpRequest();
+//         request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+//         request2.send();
 
-        request2.addEventListener('load', function () {
-            // console.log(this.responseText);
-            const data2 = JSON.parse(this.responseText);
-            renderCountryInHTML(data2, 'neighbour');
-        });
-    });
-};
+//          // NOTE: this is callback hell!!!! writing a callback function in another callback function which results in triangular shape with confusion.
+//         request2.addEventListener('load', function () {
+//             // console.log(this.responseText);
+//             const data2 = JSON.parse(this.responseText);
+//             renderCountryInHTML(data2, 'neighbour');
+//         });
+//     });
+// };
 
-getCountryAndNeighbour('usa');
+// getCountryAndNeighbour('usa');
+
+/////////////////////////////////////////////////////////
+//TODO Promise
+// NOTE: best thing to run out from callback hell (the triangular code shape with confusion)
+
+// const getCountryData = function (country) {
+//     fetch(`https://restcountries.com/v2/name/${country}`)
+//         .then(function (response) {
+//             console.log('response: ', response);
+//             return response.json(); // which is another promise, so we should use another 'then' method
+//         })
+//         .then(function (data) {
+//             console.log('data: ', data);
+//             renderCountryInHTML(data[0]);
+//         });
+// }; // NOTE: the simpler version 👇
+
+// const getCountryData = function (country) {
+//     fetch(`https://restcountries.com/v2/name/${country}`)
+//         .then(response => response.json())
+//         .then(data => renderCountryInHTML(data[0]));
+// };
+// getCountryData('germany');
+
+// const getCountryDataAndNeighbour = function (country) {
+//     fetch(`https://restcountries.com/v2/name/${country}`)
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data);
+//             renderCountryInHTML(data[0]);
+//             const neighbour = data.borders?.[0];
+//             return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+//         })
+//         .then(response => response.json())
+//         .then(data => renderCountryInHTML(data, 'neighbour'));
+// };
+
+// getCountryDataAndNeighbour('germany');
+
+
+    .then(data => console.log(data[0]));
