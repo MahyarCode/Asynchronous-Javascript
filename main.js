84b2ffc27afe742abf04b4886fdab63cc3d68425
Mahyar Mousavinia <mahyar.mousavi.1408@gmail.com>
2025-11-24 10:39:44 +0400

    restcountries api can be called now, the main.js is now working fine and start to solve the challenge

---
+++ main.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+// NEW COUNTRIES API URL (use instead of the URL shown in videos):
+// https://restcountries.com/v2/name/portugal
+
+// NEW REVERSE GEOCODING API URL (use instead of the URL shown in videos):
+// https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
+///////////////////////////////////////
+//TODO adding country details to HTML
+const renderCountryInHTML = function (data, className = '') {
+    const html = `
+    <article class="country ${className}">
+        <img class="country__img" src="${data.flag}" />
+        <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(+data.population / 1_000_000).toFixed(
+                1
+            )}M people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+        </div>
+    </article>
+    `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+};
+
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+    return fetch(url, {
+        method: 'GET',
+    }).then(response => {
+        if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+        return response.json();
+    });
+};
+
+const getCountryDataAndNeighbour = function (country) {
+    getJSON(`https://restcountries.com/v2/name/${country}`, 'Country Not Found')
+        .then(data => {
+            renderCountryInHTML(data[0]);
+            const neighbour = data[0]?.borders?.[0];
+            return getJSON(
+                `https://restcountries.com/v2/alpha/${neighbour}`,
+                'Neighbour Country Not Found'
+            );
+        })
+        .then(data => renderCountryInHTML(data, 'neighbour'))
+        .catch(err => console.error(`🔴 ${err} 🔴`));
+};
+
+btn.addEventListener('click', function () {
+    getCountryDataAndNeighbour('Australia');
+});
--- script.js
+++ script.js
@@ -3,56 +3,67 @@ import { authKey } from './API-KEY.js';
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
-const getCountry = function (country) {
+////////////////////////////////////////////////////////////
+
+const getJSON = function (url, errorMsg = 'Something went wrong') {
     const apiKey = authKey;
-    const urlCountry = `https://api.api-ninjas.com/v1/country?name=${country}`;
-    fetch(urlCountry, {
+    return fetch(url, {
         method: 'GET',
         headers: {
             'X-Api-Key': apiKey,
             'Content-Type': 'application/json',
         },
-    })
-        .then(response => response.json())
+    }).then(response => {
+        if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+        return response.json();
+    });
+};
+
+const getFlag = function (data) {
+    return getJSON(`https://api.api-ninjas.com/v1/countryflag?country=${data.iso2}`);
+};
+
+const renderHTML = function (data) {
+    const html = `
+        <article class="country">
+            <div class="country__data">
+                <h3 class="country__name">${data.name}</h3>
+                <h4 class="country__region">${data.region}</h4>
+                <p class="country__row"><span>👫</span>${(+data.population / 1_000).toFixed(
+                    1
+                )}M people</p>
+                <p class="country__row"><span>🏢</span>${data.capital}</p>
+                <p class="country__row"><span>💰</span>${data.currency.name}</p>
+            </div>
+        </article>
+    `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+};
+
+const renderImg = function (data) {
+    const html = `<img class="country__img" src="${data.rectangle_image_url}" />`;
+    document.querySelector('.country').insertAdjacentHTML('afterbegin', html);
+};
+
+////////////////////////////////////////////////////
+
+const getCountry = function (country) {
+    getJSON(`https://api.api-ninjas.com/v1/country?name=${country}`)
         .then(result => {
             const [data] = result;
-            console.log('country info: ', data);
-            const html = `
-            <article class="country">
-                <div class="country__data">
-                    <h3 class="country__name">${data.name}</h3>
-                    <h4 class="country__region">${data.region}</h4>
-                    <p class="country__row"><span>👫</span>${(+data.population / 1_000).toFixed(
-                        1
-                    )}M people</p>
-                    <p class="country__row"><span>🏢</span>${data.capital}</p>
-                    <p class="country__row"><span>💰</span>${data.currency.name}</p>
-                </div>
-            </article>
-        `;
-            countriesContainer.insertAdjacentHTML('beforeend', html);
-            countriesContainer.style.opacity = 1;
-
-            const urlFlag = `https://api.api-ninjas.com/v1/countryflag?country=${data.iso2}`;
-            const requestFlag = fetch(urlFlag, {
-                method: 'GET',
-                headers: {
-                    'X-Api-Key': apiKey,
-                    'Content-Type': 'application/json',
-                },
-            });
-            return requestFlag;
+            // console.log('country info: ', data);
+            renderHTML(data);
+            return getFlag(data);
         })
-        .then(response => response.json())
         .then(result => {
             const data = result;
-            console.log('country flag: ', data);
-
-            const html = `<img class="country__img" src="${data.rectangle_image_url}" />`;
-            document.querySelector('.country').insertAdjacentHTML('afterbegin', html);
+            // console.log('country flag: ', data);
+            renderImg(data);
         })
         .catch(err => console.error(`🔴 ${err} 🔴`));
 };
+
 btn.addEventListener('click', function () {
     getCountry('Iran');
 });
